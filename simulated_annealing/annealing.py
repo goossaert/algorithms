ef9017e46b7e650ea4b14c26d40a13f20a36a433
Emmanuel Goossaert <emmanuel.goossaert@gmail.com>
2010-04-07 14:02:07 +0200

    Improved simulated annealing.
    Simulated annealing is now improved as distances are cached,
    and as new total distances are computed by changing only the cities
    that are swapped.

--- simulated_annealing/annealing.py
+++ simulated_annealing/annealing.py
@@ -37,15 +37,17 @@ import time
 import matplotlib.pyplot as plt
 
 
+distances_pair = []
+
+
 def read_cities(filename):
     """
     Read city data from a csv file.
     """
     reader = csv.reader(open(filename, "rb")) # may raise IOError
     rows = [line for line in reader]
-    cities = [City(r[2], r[3], float(r[0]), float(r[1])) for r in rows[1:]]
+    cities = [City(r[2], index, r[3], float(r[0]), float(r[1])) for index, r in enumerate(rows[1:])]
     return cities
-
 
 
 class City:
@@ -53,20 +55,21 @@ class City:
     Store information regarding a city, including name and gps coordinates.
     """
 
-    def __init__(self, name='', description='', latitude=0, longitude=0):
+    def __init__(self, name='', index=0, description='', latitude=0, longitude=0):
         self.name = name
         self.description = description
         self.latitude = latitude
         self.longitude = longitude
+        self.index = index
 
     def __str__(self):
-        return self.name + ' ' + self.description + ' ' + str(self.latitude) + ' ' + str(self.longitude)
+        return '%s %d %s %f %f' % (self.name, self.index, self.description, self.latitude, self.longitude)
 
     def __repr__(self):
         return self.__str__()
 
-    def distance_to_city_in_km(self, city):
-        """Distance to another city using Haversine formula"""
+    def compute_distance_to_city_in_km(self, city):
+        """Distance to another city using Haversine formula."""
         lat = math.radians(self.latitude - city.latitude)
         long = math.radians(self.longitude - city.longitude)
         a = math.pow(math.sin(lat/2), 2) \
@@ -76,6 +79,26 @@ class City:
         radius_earth = 6378.7 # in kilometers
         return radius_earth * c
 
+    def distance_to_city_in_km(self, city):
+        """Distance to another city using pre-computed distance."""
+        global distances_pair
+        if self.index != city.index:
+            indices = [self.index, city.index]
+            return distances_pair[max(indices)][min(indices)]
+        return 0
+
+
+def compute_distance_pairs(cities):
+    """
+    Compute all distance pairs.
+    /!\ IMPORTANT: The cities must be ordered by their indices in the list.
+    """
+    global distances_pair
+    for city_from in cities:
+        distances_pair.append([0 for r in range(city_from.index)]) # create
+        for city_to in cities[:city_from.index]:
+            distances_pair[city_from.index][city_to.index] = city_from.compute_distance_to_city_in_km(city_to)
+         
 
 def total_distance_in_km(cities):
     distances = [cities[index].distance_to_city_in_km(cities[(index + 1) % len(cities)]) for index in range(len(cities))]
@@ -135,7 +158,39 @@ def plot_distances(distances_current, figure_id, distances_best, ids_restart, nb
     plt.legend( (line_current, line_best, line_restart)[:index_legend],
                 ('Tested distance', 'Shortest distance', 'Restarting annealing process')[:index_legend],
                 loc='upper right' )
+   
+
+def compute_swap_indices(index, nb_cities):
+    index_previous = (index - 1 + nb_cities) % nb_cities
+    index_next = (index + 1) % nb_cities
+    return (index_previous, index_next)
+
+
+def distance_swap(cities, index_a, index_b):
+    """Compute the distance inferred by the two given indices."""
+    index_A = min(index_a, index_b)
+    index_B = max(index_a, index_b)
     
+    (index_A_previous, index_A_next) = compute_swap_indices(index_A, len(cities))
+    (index_B_previous, index_B_next) = compute_swap_indices(index_B, len(cities))
+  
+    distances = []
+    # These two distances are common to the two sub-cases
+    distances.append(cities[index_A_previous].distance_to_city_in_km(cities[index_A]))
+    distances.append(cities[index_B].distance_to_city_in_km(cities[index_B_next]))
+    if index_A == index_B_previous:
+        # B is following A in the list: the distance between A and B must not
+        # be counted twice.
+        # ---x---A---B---x---
+        distances.append(cities[index_A].distance_to_city_in_km(cities[index_B]))
+    else:
+        # B is not following A in the list: all distances must be counted
+        # ---x---A---x--- ... ---x---B---x---
+        distances.append(cities[index_A].distance_to_city_in_km(cities[index_A_next]))
+        distances.append(cities[index_B_previous].distance_to_city_in_km(cities[index_B]))
+
+    return sum(distances)
+
 
 def annealing(cities, temperature_begin=1.0e+300, temperature_end=.1, cooling_factor=.99, nb_iterations=1):
     """
@@ -161,19 +216,25 @@ def annealing(cities, temperature_begin=1.0e+300, temperature_end=.1, cooling_fa
             temperature = temperature_begin
             cities_current = cities_best[:]
             distance_current = distance_best
+            distance_new = distance_best
             cities_new = cities_best[:]
 
             step = 0
             while temperature > temperature_end:
                 # swap two random cities, but never touch the first city
-                # -- could be optimized by recomputing only the changed distances
                 index = random.sample(range(len(cities_new) - 1), 2)
                 index[0] += 1
                 index[1] += 1
+
+                # optimize by recomputing only the changed distances
+                swap_before = distance_swap(cities_new, index[0], index[1])
                 cities_new[index[0]], cities_new[index[1]] = cities_new[index[1]], cities_new[index[0]]
+                swap_after = distance_swap(cities_new, index[0], index[1])
+                #print swap_before, swap_after
 
                 # compute the new distance
-                distance_new = total_distance_in_km(cities_new)
+                # recomputing all is bad: distance_new = total_distance_in_km(cities_new)
+                distance_new = distance_new - swap_before + swap_after
 
                 # acceptance probability by Kirkpatrick et al.
                 diff = distance_new - distance_current
@@ -234,6 +295,7 @@ if __name__ == '__main__':
 
     time_begin = time.time()
     cities = read_cities(input)
+    compute_distance_pairs(cities)
 
     nb_cities = len(cities) if nb_cities <= 0 else nb_cities
 
